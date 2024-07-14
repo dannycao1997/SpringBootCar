e55c2119c8e7459b88eeaddf199585e0a4be62c8
Danny Cao <dannycao1997@yahoo.com>
2024-07-14 04:06:47 -0400

    create repository and controller

--- src/main/java/com/example/demo/model/Car.java
+++ src/main/java/com/example/demo/model/Car.java
@@ -15,4 +15,36 @@ public class Car {
     private String model;
     private int year;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
 }
